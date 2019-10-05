fba842891c969a89993c6effc09b9056f2d4a202
anglyan <angel.yanguas@gmail.com>
2019-10-05 13:25:07 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,25 @@
 #A copy of the GPLv2 license is provided in the root directory of
 #the source code.
 
-from distutils.core import setup
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name = 'gris',
     description = "parser of RIS and WOK bibliography file format",
-    version = '0.5.2',
+    version = '0.6.0',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author = "Angel Yanguas-Gil",
     author_email = "angel.yanguas@gmail.com",
-    download_url = "https://github.com/anglyan/gris/tarball/0.5.2",
+    download_url = "https://github.com/anglyan/gris/tarball/0.6.0",
     url = "https://github.com/anglyan/gris.git",
-    py_modules = ['gris'],
-    package_dir = {'' : 'gris'},
-    classifiers = ["Development Status :: 3 - Alpha",
+    py_modules=["gris"],
+    python_requires=">=3",
+    classifiers = ["Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
         "Environment :: Console",
         "Topic :: Scientific/Engineering",
