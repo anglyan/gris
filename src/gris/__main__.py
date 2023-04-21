3b0c3ab5497c52b88fb254df991e8dff9868fe97
anglyan <angel.yanguas@gmail.com>
2023-04-21 00:44:26 -0500

    added __main__.py

--- src/gris/__main__.py
+++ src/gris/__main__.py
@@ -2,4 +2,10 @@ from .gris import read_ris
 
 import sys
 
-r = read_ris(sys.argv[1], False)
+rlist = read_ris(sys.argv[1], False)
+if len(sys.argv) > 2:
+    tags = sys.argv[2:]
+    for ref in rlist:
+        print(",".join([ref.get(tag,'NA') for tag in tags]))
+
+
