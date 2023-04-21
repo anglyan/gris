4a555a658785dbf94e0754f0800fbc2af6aef8c1
anglyan <angel.yanguas@gmail.com>
2023-04-21 01:38:22 -0500

    budding documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'gris'
+copyright = '2013, Angel Yanguas-Gil'
+author = 'Angel Yanguas-Gil'
+release = '0.7.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'nature'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,24 @@
+.. gris documentation master file, created by
+   sphinx-quickstart on Fri Apr 21 01:29:59 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+gris documentation
+==================
+
+gris is a lightweight parser for RIS files.
+
+Table of contents
+-----------------
+.. toctree::
+   :maxdepth: 2
+   apidocs/documentation
+
+
+
+Indices and tables
+------------------
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
